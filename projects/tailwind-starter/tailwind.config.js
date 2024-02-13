2c1b64bc5dd1fc7ce9a48a1a5e073398ebc30035
Bhushan G Y <bhushan.benz@gmail.com>
2024-02-13 18:38:11 +0530

    email subscribe card using tailwind

---
+++ projects/tailwind-starter/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'], // Look for tailwind utility classes in all html files in the root folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
